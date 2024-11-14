1cd41780b5e6d8c9df247c8c43c14c8e281ef790
Miguel Angel Fernandez <miguelangelfb19@utp.edu.co>
2024-11-14 15:13:19 -0500

    updated

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -5,6 +5,7 @@ let prisma: PrismaClient;
 if (process.env.NODE_ENV === 'production') {
   prisma = new PrismaClient();
 } else {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   if (!(global as any).prisma) {
     (global as any).prisma = new PrismaClient();
   }
