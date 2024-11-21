e97590db76a2dcbe2969b70c78f2192a4b20baf0
Miguel Angel Fernandez <miguelangelfb19@utp.edu.co>
2024-11-21 17:48:58 -0500

    updates

---
+++ next.config.mjs
@@ -0,0 +1,15 @@
+
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'tailus.io',
+            },
+        ],
+    }
+};
+
+export default nextConfig;
