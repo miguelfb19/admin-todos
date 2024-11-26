43c591b9aa748e7bd12dae598ffbec2001117026
Miguel Angel Fernandez <miguelangelfb19@utp.edu.co>
2024-11-26 16:14:24 -0500

    updates

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,4 +1,3 @@
-import { AuthProvider } from "@/auth";
 import { WidgetItem } from "@/components";
 
 export const metadata = {
@@ -7,17 +6,13 @@ export const metadata = {
 };
 
 export default async function DashboardPage() {
-
   return (
     <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-3">
       <WidgetItem title="Bienvenido a admin-todos">
         <div className="flex flex-col justify-center items-center w-full">
           <h3 className="text-xl font-bold text-gray-700 text-center overflow-scroll">
-            {/* {session.user?.name ?? "No hay usuario"} */}
             <br />
-            <span className="text-sm text-gray-500">
-              {/* {session.user?.email ?? "ejemple@ejemplo.com"} */}
-            </span>
+            <span className="text-sm text-gray-500"></span>
           </h3>
         </div>
       </WidgetItem>
